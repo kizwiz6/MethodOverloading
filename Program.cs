7c300135e0c4ac781d4525a126cd4f368b0f9270
Kieran Emery <kizwiz@hotmail.co.uk>
2024-02-21 16:23:41 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NamePets("Kieran", "Jason");
+            NamePets("Colin", "Celia", "Cheryl");
+            NamePets();
+        }
+
+        static void NamePets(string name, string name2)
+        {
+            Console.WriteLine($"Your pets {name} and {name2} will be joining your voyage across space!");
+        }
+
+        static void NamePets(string name, string name2, string name3)
+        {
+            Console.WriteLine($"Your pets {name}, {name2}, and {name3} will be joining your voyage across space!");
+        }
+
+        static void NamePets()
+        {
+            Console.WriteLine("Aw, you have no spacefaring pets!");
+        }
+    }
+}
